3b10bb73ee954c21d3f028c8828703d445e64035
AeonX <kedaren.dorasami@gmail.com>
2020-05-17 13:14:58 +0400

    modified cors

--- server.js
+++ server.js
@@ -35,25 +35,9 @@ app.use(
   })
 );
 
-const whitelist = ["http://127.0.0.1"]; // list of allow domain
+app.options("*", cors());
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) {
-      return callback(null, true);
-    }
-
-    if (whitelist.indexOf(origin) === -1) {
-      var msg =
-        "The CORS policy for this site does not " +
-        "allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
